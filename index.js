3b0740318dbfd5369d9388639947eddbbe18c68e
bapangit <cmandal97531@gmail.com>
2021-11-14 17:46:23 +0530

    adding nodemon and testing on heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 const port = process.env.PORT || 8080
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   res.send('Hello World, Good Evening !');
 })
 
 var server = app.listen(port, function () {
