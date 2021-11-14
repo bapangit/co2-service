5b6faa8613791ad493748817fe7161a11e0cf9d5
bapangit <cmandal97531@gmail.com>
2021-11-14 16:57:50 +0530

    adding script for heroku

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require('http');
 
+const port = process.env.PORT || 3000
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end('Good Morning, All Friends!');
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(port);
