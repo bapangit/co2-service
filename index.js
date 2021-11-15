6dcdd0dd6dee940af240f66ee97ac96b95557826
bapangit <cmandal97531@gmail.com>
2021-11-15 23:20:51 +0530

    adding google authentication

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 var express = require('express');
+const passport = require('passport')
 var app = express();
+app.use(passport.initialize());
+require('./auth')
+
+  app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+  app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
+
+  
+
 const port = process.env.PORT || 8080
 
 app.get('/', function (req, res) {
    res.send('Good Morning !');
+})
+
+app.get('/success', function (req, res) {
+   res.send('Loggedin !');
+   console.log("logged in")
+})
+
+app.get('/error', function (req, res) {
+   res.send('Login Failed !');
+   console.log("did not login")
 })
 
 var server = app.listen(port, function () {
