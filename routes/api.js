5ab9beccff988bd4eff07b8e655457d01ac54f63
bapangit <cmandal97531@gmail.com>
2021-12-22 12:54:42 +0530

    testing heroku

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const router = express.Router()
   */
 const {refreshTokenController} = require('../controlers/apis/refreshToken')
 const {myPhotos} = require('../controlers/apis/myPhotos')
+const { deletePhoto } = require('../controlers/apis/deletePhoto');
 const { publish } = require('../controlers/apis/publish');
 const { unPublish } = require('../controlers/apis/unPublish');
 const { getUser } = require('../controlers/apis/getUser');
@@ -20,6 +21,7 @@ router.post("/refresh", refreshTokenController)
 authorized routes
  */
 router.post("/myphotos",isAuthorized,myPhotos)
+router.post("/deletephoto",isAuthorized,deletePhoto)
 router.post("/publish",isAuthorized,publish)
 router.post("/unpublish",isAuthorized,unPublish)
 router.post("/getuser",isAuthorized,getUser)
