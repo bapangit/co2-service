850e47d9d229a68be8bed86a723234b00c8a440f
bapangit <cmandal97531@gmail.com>
2021-12-22 13:39:37 +0530

    testing heroku

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,9 @@ const router = express.Router()
 /*
  controllers
   */
-const {refreshTokenController} = require('../controlers/apis/refreshToken')
-const {uploadImageController} = require('../controlers/apis/uploadImage')
-const {myPhotos} = require('../controlers/apis/myPhotos')
+const { refreshTokenController } = require('../controlers/apis/refreshToken')
+const { uploadImageController } = require('../controlers/apis/uploadImage')
+const { myPhotos } = require('../controlers/apis/myPhotos')
 const { deletePhoto } = require('../controlers/apis/deletePhoto');
 const { publish } = require('../controlers/apis/publish');
 const { unPublish } = require('../controlers/apis/unPublish');
@@ -14,7 +14,7 @@ const { getUser } = require('../controlers/apis/getUser');
  middlewares
   */
 const { isAuthorized } = require("../middlewares/auth.js")
-const {uploadImageMiddleware} = require("../utils/uploadimage");
+const { uploadImageMiddleware } = require("../utils/uploadimage");
 /* 
 non authorized routes
  */
@@ -23,10 +23,10 @@ router.post("/refresh", refreshTokenController)
 authorized routes
  */
 router.post("/uploadimage", isAuthorized, uploadImageMiddleware, uploadImageController)
-router.post("/myphotos",isAuthorized,myPhotos)
-router.post("/deletephoto",isAuthorized,deletePhoto)
-router.post("/publish",isAuthorized,publish)
-router.post("/unpublish",isAuthorized,unPublish)
-router.post("/getuser",isAuthorized,getUser)
+router.post("/myphotos", isAuthorized, myPhotos)
+router.post("/deletephoto", isAuthorized, deletePhoto)
+router.post("/publish", isAuthorized, publish)
+router.post("/unpublish", isAuthorized, unPublish)
+router.post("/getuser", isAuthorized, getUser)
 
 module.exports = router
